90f2c34a266efa58daabeb8e2e9a21a2962d6ac0
DragonZero000 <psp2game@mail.ru>
2026-02-22 17:29:15 +0300

    aut partly completed

--- backend/authorization.py
+++ backend/authorization.py
@@ -1,40 +1,46 @@
-import secrets
+# import secrets
 from datetime import datetime, timedelta, timezone
 from typing import Optional
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi.middleware.cors import CORSMiddleware
-from jose import JWTError, jwt
-from pydantic_settings import BaseSettings
-from pydantic import BaseModel
-import db
-from db import User
-from argon2 import PasswordHasher, exceptions
+
 import argon2
+import db
+from argon2 import PasswordHasher, exceptions
+from db import User
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     SECRET_KEY: str = "hs256"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
+
 class UserCreate(BaseModel):
     login: str
     password: str
+
 
 settings = Settings()
 
 ph = PasswordHasher(
     time_cost=2,
-    memory_cost=102400,     # 100 MiB
+    memory_cost=102400,  # 100 MiB
     parallelism=8,
     hash_len=32,
     salt_len=16,
-    encoding='utf-8',
-    type=argon2.Type.ID,    # Argon2id
+    encoding="utf-8",
+    type=argon2.Type.ID,  # Argon2id
 )
+
 
 def get_password_hash(password: str) -> str:
     return ph.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     try:
@@ -45,23 +51,31 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     except exceptions.InvalidHashError:
         return False
 
+
 def create_user(user: UserCreate):
     hashed_password = get_password_hash(user.password)
     db.insert_user_data(user.login, hashed_password)
     return True
 
+
 # JWT функции
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
     return encoded_jwt
+
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -70,7 +84,9 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
@@ -81,16 +97,19 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return user
 
+
 # FastAPI приложение
 app = FastAPI()
 ori = ["http://localhost:54718", "http://127.0.0.1:54718"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],            # Разрешает запросы с этих адресов
-    allow_credentials=False,           # Разрешает передачу cookies и заголовков авторизации
-    allow_methods=["*"],              # Разрешает все методы (GET, POST, OPTIONS и т.д.)
-    allow_headers=["*"],              # Разрешает все заголовки
+    allow_origins=["*"],  # Разрешает запросы с этих адресов
+    allow_credentials=False,  # Разрешает передачу cookies и заголовков авторизации
+    allow_methods=["*"],  # Разрешает все методы (GET, POST, OPTIONS и т.д.)
+    allow_headers=["*"],  # Разрешает все заголовки
 )
+
+
 @app.post("/register")
 def register(user: UserCreate):
     db_user = db.get_user_data(user.login)
@@ -100,6 +119,7 @@ def register(user: UserCreate):
     if not new_user:
         raise HTTPException(status_code=400, detail="Failed to create user")
     return {"msg": "User created"}
+
 
 @app.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -111,11 +131,15 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.login}, expires_delta=access_token_expires)
+    access_token = create_access_token(
+        data={"sub": user.login}, expires_delta=access_token_expires
+    )
     return {"access_token": access_token, "token_type": "bearer"}
+
 
 @app.get("/users/me")
 def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
+
 
 # Запуск: uvicorn app:app --reload
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -4,7 +4,8 @@ import axios from "axios"
 import { useNavigate } from 'react-router-dom'
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'http://127.0.0.1:8000',
+    timeout: 10000
 })
 function Login() {
     const [formData, setFormData] = useState({ login: '', password: '' });
@@ -17,27 +18,36 @@ function Login() {
     };
     const handleSubmit = async function (e) {
         e.preventDefault()
-        console.log(formData.login, formData.password)
         setError(null)
         setIsLoading(true)
         try {
-            console.log(formData.login, "yugfv1")
             const result = await api.post('/token', new URLSearchParams({ username: formData.login, password: formData.password, grant_type: "password" }),
                 {
                     headers: { "Content-Type": "application/x-www-form-urlencoded" },
                 })
-            console.log(formData.login, "yugfv")
             sessionStorage.setItem('access_token', result.data.access_token)
-            navigate('/main')
+            navigate('/main', {replace: true})
         }
         catch (err) {
-            console.log(err)
-            const errorStatus = err?.response?.status
-            setError(errorStatus === 401 ? 'Incorrect login or password' : 'Error connection')            
+            if (axios.isAxiosError(err)) {
+              const status = err.response?.status;
+              if (status === 401) {
+                setError('Incorrect login or password');
+              }
+              else if (status === 0 || !err.response) {
+                setError('No connection to the server');
+              }
+              else {
+                setError('An error occurred. Please try again later.');
+              }
+            }
+            else {
+              setError('Unknown error');
+              console.error(err);
+            }
         }
         finally {
             setIsLoading(false)
-            console.log(error, isLoading)
         }
     }
 
@@ -47,13 +57,21 @@ function Login() {
             <form onSubmit={handleSubmit} className="p-2 flex flex-col gap-1 border border-white-500 rounded-xl bg-gray-800">
                 <div className='flex gap-1'>
                     <label>Login:</label>
-                    <input type="text" name="login" value={formData.login} onChange={handleChange} placeholder="admin" required className='basis-full'></input>
+                    <input type="text" name="login" value={formData.login} onChange={handleChange} placeholder="admin" required className='basis-full' disabled={isLoading}></input>
                 </div>
                 <div className='flex gap-1'>
                     <label>Password:</label>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="qwerty" required className='basis-full'></input>
+                    <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="qwerty" required className='basis-full' disabled={isLoading}></input>
                 </div>
-                <button type="submit" className='border border-white-500 rounded-xl'>Enter</button>
+                {error && (<div className="rounded-xl bg-red-900/50 text-sm text-red-200 text-center">{error}</div>)}
+                <div className="flex items-center justify-center">
+                <button type="submit" className='w-full flex items-center justify-center border border-white-500 rounded-xl' disabled={isLoading}>{isLoading ? (<>
+                  <svg className="mr-2 h-5 w-5 animate-spin" viewBox="0 0 24 24" fill="none">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>Entering...</>) : ('Enter')
+                }</button>
+                </div>
             </form>
         </div>
     )
